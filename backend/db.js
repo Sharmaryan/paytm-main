de4e75c1826a2d8b28ae40eb29d5f0ec23514a2a
sharmaryan <panditaryan.as@gmail.com>
2024-09-26 21:05:37 +0530

    refactor: db schema

--- backend/db.js
+++ backend/db.js
@@ -1,14 +1,34 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(
-  ""
-);
+mongoose.connect("");
 
 const userSchema = new mongoose.Schema({
-  firstName: String,
-  lastName: String,
-  password: String,
-  username: String,
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+    maxLength: 50,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+    maxLength: 50,
+  },
+  password: {
+    type: String,
+    requred: true,
+    minLength: 6,
+  },
+  username: {
+    type: String,
+    required: true,
+    trim: true,
+    uniqure: true,
+    lowercase: true,
+    minLength: 3,
+    maxLength: 30,
+  },
 });
 
 const User = mongoose.model("User", userSchema);
