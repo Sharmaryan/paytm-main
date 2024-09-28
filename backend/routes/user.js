3120c13774d7ae47c457a39429352a4e5a0e87fc
sharmaryan <panditaryan.as@gmail.com>
2024-09-28 18:35:31 +0530

    feat : created signup/signin routes

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,4 +1,63 @@
 const express = require("express");
 const router = express.Router();
+const zod = require("zod");
+const jwt = require("jsonwebtoken");
+const { User } = require("../db");
+const JWT_SECRET = require("../config");
+
+const userSchema = zod.object({
+  username: zod.string().min(3).max(30).email(),
+  firstName: zod.string().max(50),
+  lastName: zod.string().max(50),
+  password: zod.string().min(6),
+});
+
+const signinBody = zod.object({
+  username: zod.string().email(),
+  password: zod.string(),
+});
+
+router.post("/signup", async (req, res) => {
+  const payload = req.body;
+  const username = payload.username;
+  const data = userSchema.safeParse(payload);
+  if (data.success) {
+    const userExists = await User.find({ username });
+    if (!!userExists?.length) {
+      res.status(411).json({ message: "Email already taken" });
+    } else {
+      try {
+        const newUser = await User.create(payload);
+        const token = jwt.sign({ userId: newUser._id }, JWT_SECRET);
+        res.status(200).json({
+          message: "User created successfully",
+          token,
+        });
+      } catch (err) {
+        res.status(411).json({ message: "user not created, try again" });
+      }
+    }
+  } else {
+    res.status(411).json({ message: "Incorrect Inputs" });
+  }
+});
+
+router.post("/signin", async (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  const { success } = signinBody.safeParse(req.body);
+  if (!success) {
+    return res.status(411).json({
+      message: "Incorrect inputs",
+    });
+  }
+  const user = await User.findOne({ username, password });
+  if (user?.id) {
+    const token = jwt.sign({ userId: user._id }, JWT_SECRET);
+    res.status(200).json({ token });
+  } else {
+    res.status(411).json({ message: "Error while logging in" });
+  }
+});
 
 module.exports = router;
